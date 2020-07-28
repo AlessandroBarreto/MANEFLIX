5a8f04ea66b5effae2fc67276cc6df1cb5e3e399
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2020-07-28 19:40:17 -0300

    Inicio roteamento

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/Home/App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de Video
+    </div>
+  )
+}
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />  
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
